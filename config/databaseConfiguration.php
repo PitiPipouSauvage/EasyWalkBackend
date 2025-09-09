d1cd28923eb6d14034622fb21244ec68ad00c7c5
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2025-09-09 15:19:31 +0200

    feat: database connexion and configuration

---
+++ config/databaseConfiguration.php
@@ -0,0 +1,19 @@
+<?php
+
+class databaseConfiguration
+{
+    static private array $databaseConfiguration = array();
+
+    public static function init(): void {
+        self::$databaseConfiguration["hostname"] = getenv("DB_HOST");
+        self::$databaseConfiguration["dbName"] = getenv("DB_NAME");
+        self::$databaseConfiguration["dbPort"] = getenv("DB_PORT");
+        self::$databaseConfiguration["login"] = getenv("DB_USER");
+        self::$databaseConfiguration["password"] = getenv("DB_PASS");
+    }
+
+    static public function getDatabaseConfiguration(): array {
+        self::init();
+        return self::$databaseConfiguration;
+    }
+}
---
+++ config/databaseDatabaseConnexion.php
@@ -0,0 +1,25 @@
+<?php
+require_once "databaseDatabaseConnexion.php";
+
+class databaseConnexion {
+    private static ?databaseConnexion $instance = null;
+    private PDO $pdo;
+
+    private function __construct() {
+        $config = databaseConfiguration::getDatabaseConfiguration();
+        $pdo = new PDO("mysql:host=$config[nomHote];port=$config[portBaseDeDonnees];dbname=$config[nomBaseDeDonnees]",
+            $config["login"], $config["motDePasse"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $this->pdo = $pdo;
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public static function getPdo(): PDO {
+        return databaseConnexion::getInstance()->pdo;
+    }
+
+    private static function getInstance(): databaseConnexion {
+        if (is_null(databaseConnexion::$instance)) databaseConnexion::$instance = new databaseConnexion();
+        return databaseConnexion::$instance;
+    }
+}
+
