c35f8bbde37e78a2d98d4d9482571919ad6924d9
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2025-09-10 10:42:50 +0200

    feat: added two tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -14,5 +14,29 @@ class UserTest extends TestCase
         }
         $isUserCreated = $user->userExists("helloKitty");
         $this->assertTrue($isUserCreated);
+        Utils::removeUser("helloKitty");
+    }
+
+    public function testLoginUser() {
+        $user = User::getInstance();
+        if (!$user->userExists("helloKitty")) {
+            $user->createUser("helloKitty", "I love kittens");
+        }
+        $this->assertTrue($user->verify("helloKitty", "I love kittens"));
+        Utils::removeUser("helloKitty");
+    }
+
+    public function testFriends() {
+        $user = User::getInstance();
+        if (!$user->userExists("helloKitty")) {
+            $user->createUser("helloKitty", "I love kittens");
+        }
+        if (!$user->userExists("dora")) {
+            $user->createUser("dora", "hola soy dora");
+        }
+
+        $user->addFriend("helloKitty", "dora");
+        $this->assertEquals(["dora"], $user->getFriends("helloKitty"));
+        $this->assertEquals(["helloKitty"], $user->getFriends("dora"));
     }
 }
