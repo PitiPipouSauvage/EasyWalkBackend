e14e3b93c7dda92af6c967a8cbeac0ab0d5decc6
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2025-09-10 09:26:22 +0200

    fix: changed wrong index

--- src/config/databaseConnexion.php
+++ src/config/databaseConnexion.php
@@ -7,8 +7,8 @@ class databaseConnexion {
 
     private function __construct() {
         $config = databaseConfiguration::getDatabaseConfiguration();
-        $pdo = new PDO("mysql:host=$config[nomHote];port=$config[portBaseDeDonnees];dbname=$config[nomBaseDeDonnees]",
-            $config["login"], $config["motDePasse"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $pdo = new PDO("mysql:host=$config[hostname];port=$config[dbPort];dbname=$config[dbName]",
+            $config["login"], $config["password"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $this->pdo = $pdo;
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
