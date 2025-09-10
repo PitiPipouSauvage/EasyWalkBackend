711bd2db82241f1b331e5cea5cc57ad6a4c42472
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2025-09-10 11:00:43 +0200

    feat: added auth route

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,3 +5,4 @@ $router->post('/users', 'UserController@store');
 $router->get('/users/{username}', 'UserController@show');
 $router->get('/users/addFriend/{username}', 'UserController@addFriend');
 $router->get('/users/friendList/{username}', 'UserController@getFriends');
+$router->post('/users/auth/{username}/{password}', 'UserController@login');
