cc8a31de157aa4c0f0f669eca22d97ffda16ae42
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2025-09-10 10:27:33 +0200

    feat: created router to handle requests

---
+++ src/core/Router.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace core;
+
+class Router
+{
+    private array $routes = [];
+
+    public function get($path, $action) {
+        $this->addRoute('GET', $path, $action);
+    }
+
+    public function post($path, $action) {
+        $this->addRoute('POST', $path, $action);
+    }
+
+    public function addRoute($method, $path, $action) {
+        $pattern = preg_replace('#\{\w+}#', '([\w-]+)', $path);
+        $pattern = "#^" . $pattern . "$#";
+        $this->routes[] = compact('method', 'pattern', 'action');
+    }
+
+    public function dispatch($uri, $method) {
+        foreach ($this->routes as $route) {
+            if ($route['method'] === $method && preg_match($route['pattern'], $uri, $matches)) {
+                array_shift($matches);
+                return $this->callAction($route['action'], $matches);
+            }
+        }
+        http_response_code(404);
+        echo json_encode(['message' => 'Not Found']);
+        exit;
+    }
+
+    private function callAction($action, array $params) {
+        list($controllerName, $method) = explode('@', $action);
+        require_once $controllerName . '.php';
+        $controller = new $controllerName();
+        return call_user_func_array([$controller, $method], $params);
+    }
+}
