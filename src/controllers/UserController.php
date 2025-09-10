966f5668099cd9e0482a85b1c870ea6da9a9013d
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2025-09-10 10:27:59 +0200

    feat: added UserController and entry point

---
+++ src/controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+use core\Controller;
+
+require_once "src/models/User.php";
+
+class UserController extends Controller
+{
+    public function index() {
+        $this->json_response(User::getInstance()->all());
+    }
+
+    public function show($username) {
+        if (!User::getInstance()->userExists($username)) {
+            $this->json_error("User doesn't exist");
+        }
+        $this->json_response(User::getInstance()->get($username));
+    }
+
+    public function getFriends($username) {
+        if (!User::getInstance()->userExists($username)) {
+            $this->json_error("User doesn't exist");
+        }
+        $this->json_response(User::getInstance()->getFriends($username));
+    }
+
+    public function create($username, $password) {
+        if (!User::getInstance()->userExists($username)) {
+            User::getInstance()->createUser($username, $password);
+        } else {
+            $this->json_error("User already exists");
+        }
+    }
+
+    public function addFriend($username, $friendUsername) {
+        if (!User::getInstance()->userExists($username) || !User::getInstance()->userExists($friendUsername)) {
+            $this->json_error("User doesn't exist");
+        }
+        User::getInstance()->addFriend($username, $friendUsername);
+    }
+
+    public function rateUser($username, $rating, $description, $friendUsername) {
+        if (!User::getInstance()->userExists($username) || !User::getInstance()->userExists($friendUsername)) {
+            $this->json_error("User doesn't exist");
+        }
+        User::getInstance()->rateUser($username, $rating, $description,$friendUsername);
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use core\Router;
+
+require_once "core/Router.php";
+require_once "core/Controller.php";
+
+$router = new Router();
+
+require_once "routes/api.php";
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+$router->dispatch($method, $uri);
