b4503040cbe9fdde8d09fb18eb4e2e2d2a240b44
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2025-09-10 11:10:31 +0200

    fix: changed path order

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-$router->get('/users', 'UserController@index');
 $router->post('/users', 'UserController@store');
 $router->get('/users/{username}', 'UserController@show');
 $router->get('/users/addFriend/{username}', 'UserController@addFriend');
 $router->get('/users/friendList/{username}', 'UserController@getFriends');
 $router->post('/users/auth/{username}/{password}', 'UserController@login');
+$router->get('/users', 'UserController@index');
